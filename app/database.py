dfc91965b0f646dfa8aa56e76bd20adba17feae2
dheer31 <dheerajd2003@gmail.com>
2026-09-18 22:24:39 +0530

    done

--- app/database.py
+++ app/database.py
@@ -4,14 +4,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = os.getenv("MYSQL_USER", "app_user")
-DB_PASSWORD = os.getenv("MYSQL_PASSWORD", "app_password")
-DB_HOST = os.getenv("MYSQL_HOST", "db")
-DB_PORT = os.getenv("MYSQL_PORT", "3306")
-DB_NAME = os.getenv("MYSQL_DATABASE", "app_db")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+
 
 SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
 )
 
 # pool_pre_ping avoids "MySQL server has gone away" errors on idle connections
